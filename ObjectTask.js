6647b7626e87a1564250d4dbb9f1afbf5be43911
Kuse_design <0luwakusejerry1@gmail.com>
2026-02-16 10:35:18 +0100

    ObjectTask

---
+++ ObjectTask.js
@@ -0,0 +1,20 @@
+const person = {
+    firstName: "jerry",
+    lastName : "oluwakuse",
+    age: "45"
+
+    }
+person.email = "ogundey@gmail.com"
+person.age = "25"
+delete person.lastName
+
+console.log(person)
+
+
+
+
+
+
+
+
+
---
+++ ObjectTaskTwo.js
@@ -0,0 +1,17 @@
+const student = {
+    name: "Treasure",
+    age: "50",
+    grade: "A"
+}
+student.state = "Ondo State"
+console.log(student)
+delete student.age
+console.log(Object.keys(student))
+console.log(Object.values(student))
+
+
+
+
+
+
+
